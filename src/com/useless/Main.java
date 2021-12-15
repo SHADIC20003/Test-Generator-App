4f392d8e4730486f4d58a7789c866026110d1f39
Sherifrax <a.rahmansherif412004@gmail.com>
2021-12-15 21:02:26 +0200

    testing3

--- src/com/useless/Main.java
+++ src/com/useless/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("is this working?");
         System.out.println("i am awesome");
+        System.out.println("I am more awesome than you");
     }
 }
