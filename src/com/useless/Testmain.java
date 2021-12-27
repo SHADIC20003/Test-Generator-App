1f8e9d33827fc406c6ca71646322c5e3a531dbf3
AhmedAlaa500 <ahmed.alaa500@yahoo.com>
2021-12-28 00:44:15 +0200

    a

--- src/com/useless/Testmain.java
+++ src/com/useless/Testmain.java
@@ -1,122 +1,251 @@
-package project3;
 
+package project3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Testmain {
-	
-	public static void main(String[] args) {
-		
-String FilePathTests=("Number of tests.txt");
-String FilePathQuestions=("number of Questions");
-		int number_of_tests=Functions.Read_Counters(FilePathTests);
-		
-		
-	int y,z;
-			Scanner input = new Scanner(System.in);
-            WorkingWithFiles Math = new WorkingWithFiles("MathQuestions.txt","MathAnswers.txt");
-			WorkingWithFiles Science  = new WorkingWithFiles("ScienceQuestions.txt","ScienceAnswers.txt");
-			WorkingWithFiles History = new WorkingWithFiles("HistoryQuestions.txt","HistoryAnswers.txt");
 
-	
-                       System.out.println("press 1 if you want to create a test bank or add questions ");
-                        System.out.println("press 2 to take a test ");
-                        
-			int x=input.nextInt();
-			
-			
-			
-		
-			
-             switch (x) {
-                 case 1:
-                      ArrayList<String> questions = new ArrayList<>();
-                      ArrayList<String> answers = new ArrayList<>();
-                      System.out.println("how many questions you want to add");
-                      int numOfQuestions=Functions.Read_Counters(FilePathQuestions);
-                      numOfQuestions+=input.nextInt();
-                      Functions.Write_Counters(numOfQuestions,FilePathQuestions);
-                      
-                      Functions.fillArrayList_TestBank( questions,answers,Functions.Read_Counters(FilePathQuestions));
-                        
-                      System.out.println("you have three subjects to choose from them \n");
-			System.out.println("press 1 for Maths\n");
-                        System.out.println("press 2 for Science \n");
-                        System.out.println("press 3 for History \n");
-                         
-                       
-                        y=input.nextInt();
-                               switch(y){
-                                   case 1:
-                                   Functions.copy_arraylist_to_files(questions,answers, Functions.Read_Counters(FilePathQuestions),Math.getFilepathQ(),Math.getFilepathA());
-                               break;    
-                              
-                                   case 2:
-                                    Functions.copy_arraylist_to_files(questions,answers, Functions.Read_Counters(FilePathQuestions),Math.getFilepathQ(),Science.getFilepathA());
-                               break;
-                               
-                                   case 3:
-                                    Functions.copy_arraylist_to_files(questions,answers, Functions.Read_Counters(FilePathQuestions),History.getFilepathQ(),History.getFilepathA());
-                              break;
-                               
-                                   default:
-                                    System.out.println("wrong number");
-                              break;
-                               }
-                   
-                     break;
-                 case 2:
-                	 
-                	 number_of_tests+=1;
-                	 
-                	 Functions.Write_Counters( number_of_tests, FilePathTests);
-                	 
-                      
-                        System.out.println("you have three subjects to choose from them \n");
-			System.out.println("press 1 for Maths\n");
-                        System.out.println("press 2 for Science \n");
-                        System.out.println("press 3 for History \n");
-                         
-                        z=input.nextInt();
-                        
-                    
-                               switch(z){
-                                   case 1:
-                                	  
-                                       Functions.Exam(Math.getFilepathQ(), Math.getFilepathA(),Functions.Read_Counters(FilePathQuestions));
-                                       System.out.println("number of tests taken are"+Functions.Read_Counters(FilePathTests));
-                                   break;
-                                  
-                                   case 2:
-                                        Functions.Exam(Science.getFilepathQ(), Science.getFilepathA(),Functions.Read_Counters(FilePathQuestions));
-                                        System.out.println("number of tests taken are"+Functions.Read_Counters(FilePathTests));
-                                   break;
-                                   
-                                   case 3:
-                                        Functions.Exam(History.getFilepathQ(), History.getFilepathA(),Functions.Read_Counters(FilePathQuestions));
-                                        System.out.println("number of tests taken are"+Functions.Read_Counters(FilePathTests));
-                                   break;
-                                   
-                                   default:
-                                       System.out.println("wrong number");
-                                       break;
-                               }
-                  break;   
-                 default:
-                     System.out.println(" wrong number ");
-                     break;
-                     
-                   
-             }
-            
+public class Functions {
 
-             
-             
-}	
+	static String FilePathPass=("Number of tests.txt");
+	static String FilePathFail=("Number of tests.txt");
+     
+
+
+	public static void fillArrayList_TestBank( ArrayList<String> questions,ArrayList<String> answers,int num_of_question) {
+		try (Scanner input = new Scanner(System.in)) {
+			
+			String question;
+			String answer;
+			
+				
+					for(int i=0;i<num_of_question;i++) {
+					
+					System.out.println("type the question "+(i+1));
+					System.out.println("press enter to type the answer");
+					
+					question=input.nextLine();
+					
+					questions.add(i,question);
+					System.out.println("enter the answer of the question "+(i+1));
+					
+					
+					
+					answer=input.nextLine();
+					
+					answers.add(i,answer);
+					//then read till != that keyword  
+					
+					
+					//saveTofile( filepath,questions.get(i),true);
+					//objectIO.WriteObjectToFile(question.indent(i));
+					
+				}//for loop
+			
+		}//scanner of function fill array
+		}//function fill array
+	
+	
+public static void copy_arraylist_to_files(ArrayList<String> questions,ArrayList<String> answers,int num_of_question,String filepathquestions,String filepathanswers) {
+	
+	try{
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(filepathquestions,true));
+	    
+	    for(int i=0;i<num_of_question;i++) {
+	    
+	    	  bw.write(questions.get(i));
+	    		bw.write("\n");
+	    	  
+	    }
+	   
+	    bw.close();
+
+	}catch (Exception e1) {
+		return ;
+
+	}
+try{
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(filepathanswers,true));
+	    
+	    for(int i=0;i<num_of_question;i++) {
+	    	  bw.write(answers.get(i));
+	    		bw.write("\n");
+	    	  
+	    }
+	   
+	    bw.close();
+
+	}catch (Exception e1) {
+		return ;
+
+	}
 }
+	public static void Exam(String filepathquestions,String filepathanswers,int num_of_question) {
+		
+		int passCounter=Read_Counters(FilePathPass);
+		int failCounter=Read_Counters(FilePathFail);	
+		@SuppressWarnings("resource")
+
+			Scanner input2=new Scanner (System.in);
+
+			ArrayList<String> questionsRead = new ArrayList<>();
+			ArrayList<String> answersRead = new ArrayList<>();
+
+	try{
+	   
+			BufferedReader br = new BufferedReader(new FileReader(filepathquestions));
+	  
+		for(int i=0;i<num_of_question;i++) {
+			
+		    questionsRead.add(i,br.readLine());
+		    
+		  }
+		br.close();
+	}catch (Exception e1) {
+		return ;
+
+		}
+	try{
+		   
+		BufferedReader br = new BufferedReader(new FileReader(filepathanswers));
+
+	for(int i=0;i<num_of_question;i++) {
+	 
+		 answersRead.add(i,br.readLine());
+	  }
+
+	br.close();
+	}catch (Exception e1) {
+		return ;
+
+	}
+	if(num_of_question<10) {
+		int seed = (int)Math.floor(Math.random()*(100-50+1)+50);
+		   
+        Collections.shuffle( questionsRead,new Random(seed));
+    Collections.shuffle( answersRead,new Random(seed));
+   
+    int correct_counter=0;
+ 
+   
+  for(int i =0;i<num_of_question;i++) {
+    
+    		System.out.println(questionsRead.get(i));
+    		System.out.println("Enter the answer for the question ");
+    
+    		String answer_std=input2.nextLine();
+    		answer_std.trim();
+    
+    if(answersRead.get(i).equalsIgnoreCase(answer_std)) {
+    	correct_counter++;
+    }
+    
+   }
+   
+if (correct_counter>=5) {
+	   System.out.println("you passed this exam\n ");
+passCounter+=1;
+	Write_Counters(passCounter,FilePathPass);
+}
+else { 
+	System.out.println("you failed this exam \n"+"\nyou got only "+correct_counter );
+	failCounter+=1;
+	Write_Counters(failCounter,FilePathFail);
+	
+}
+	for(int i=0;i<num_of_question;i++) {
+	
+		System.out.println("the question is \n\n"+questionsRead.get(i));
+
+	
+		System.out.println("the correct answer is \n\n"+answersRead.get(i));
+
+		
+
+}
+	System.out.println("number of passes are"+Read_Counters(FilePathPass));
+	System.out.println("number of fails are"+Read_Counters(FilePathFail));
+	}
+else {
+	int seed = (int)Math.floor(Math.random()*(100-50+1)+50);
+	   
+            Collections.shuffle( questionsRead,new Random(seed));
+	    Collections.shuffle( answersRead,new Random(seed));
+	   
+	    int correct_counter=0;
+	 
+	   
+	  for(int i =0;i<10;i++) {
+	    
+	    		System.out.println(questionsRead.get(i));
+	    		System.out.println("Enter the answer for the question ");
+	    
+	    		String answer_std=input2.nextLine();
+	    		answer_std.trim();
+	    
+	    if(answersRead.get(i).equalsIgnoreCase(answer_std)) {
+	    	correct_counter++;
+	    }
+	    
+	   }
+	   
+	if (correct_counter>=5) {
+		   System.out.println("you passed this exam\n ");
+	passCounter+=1;
+		Write_Counters(passCounter,FilePathPass);
+	}
+	else { 
+		System.out.println("you failed this exam \n"+"\nyou got only "+correct_counter );
+		failCounter+=1;
+		Write_Counters(failCounter,FilePathFail);
+		
+	}
+		for(int i=0;i<10;i++) {
+		
+			System.out.println("the question is \n\n"+questionsRead.get(i));
+
+		
+			System.out.println("the correct answer is \n\n"+answersRead.get(i));
+
+			
+
+	}
+		System.out.println("number of passes are"+Read_Counters(FilePathPass));
+		System.out.println("number of fails are"+Read_Counters(FilePathFail));
+	}
+}
+	public static void Write_Counters(int count,String FilePath) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(FilePath));
+			bw.write(count++);
+			bw.close();
+		}catch(Exception e){
+			return ;
+		}
+	}
+		
+	public static int Read_Counters(String FilePath) {
+			int count=0;
+			try {
+			BufferedReader br=new BufferedReader(new FileReader(FilePath)) ;
+				count = br.read();
+				br.close();
+			
+		}catch(Exception e) {
+		System.out.println("");
+		}
+		return count;
+		}
+
+
+	
+}
+
